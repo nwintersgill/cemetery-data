1b81496a9597f17e92702fbcc0b624e024ab38d5
Nathan Wintersgill <njw011@bucknell.edu>
2019-12-14 23:40:23 -0500

    initialize tomcat, create index.jsp

--- src/main/java/com/launch/Main.java
+++ src/main/java/com/launch/Main.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example;
+package com.launch;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -34,6 +34,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import java.io.File;
+
 @Controller
 @SpringBootApplication
 public class Main {
@@ -46,6 +53,31 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     System.out.println("HERE");
+    String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
     SpringApplication.run(Main.class, args);
   }
 
