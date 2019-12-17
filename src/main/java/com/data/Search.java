9db00c8c6dd5187342c107fecfc92594d4eb8897
Nathan Wintersgill <njw011@bucknell.edu>
2019-12-17 16:57:31 -0500

    fix function calls

--- src/main/java/com/data/Search.java
+++ src/main/java/com/data/Search.java
@@ -266,11 +266,11 @@ public class Search {
 		if (record.equalsIgnoreCase("first")) {
 			while (!plotList.isEmpty()) {
 				Plot curPlot = plotList.remove(0);
-				System.out.println("LAST NAME: " + curPlot.getLastName);
-				System.out.println("MIDDLE NAME: " +curPlot.getMiddleName);
-				System.out.println("LOCATION: " + curPlot.getLocation);
-				System.out.println("BIRTH YEAR: " + curPlot.getBirthYear);
-				System.out.println("DEATH YEAR: " + curPlot.getDeathYear);
+				System.out.println("LAST NAME: " + curPlot.getLastName());
+				System.out.println("MIDDLE NAME: " +curPlot.getMiddleName());
+				System.out.println("LOCATION: " + curPlot.getLocation());
+				System.out.println("BIRTH YEAR: " + curPlot.getBirthYear());
+				System.out.println("DEATH YEAR: " + curPlot.getDeathYear());
 				if (lastName == null || curPlot.getLastName().equalsIgnoreCase(lastName)) {
 					System.out.println("ACCESSING MIDDLE NAME");
 					if (middleName == null || curPlot.getMiddleName().equalsIgnoreCase(middleName)) {
