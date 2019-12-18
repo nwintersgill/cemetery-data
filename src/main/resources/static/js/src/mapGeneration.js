b613ec6758011c1c6a9b25e12db992bf548ace54
Nathan Wintersgill <njw011@bucknell.edu>
2019-12-18 01:33:33 -0500

    embed map, resolve mixed security

--- src/main/resources/static/js/src/mapGeneration.js
+++ src/main/resources/static/js/src/mapGeneration.js
@@ -3,8 +3,8 @@ const colorCode = [green = '#49FF00', red = '#FF0000', yellow = '#FFD500'];
 /* setting the map at LatLng{40.9578336, -76.88758} with zoom 15*/
     function mapGen(centerPoint, zoom){
       var map = L.map('map').setView(centerPoint, zoom);
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 25
       }).addTo(map); // Set up the basic map layer, in this case the map layer is OpenStreetMap
       return map;
