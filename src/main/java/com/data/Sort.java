acf605e232ca32fd92bd9f13bc661dafa6e319c4
Nina Benson <ninabens98@gmail.com>
2019-12-17 20:35:29 -0500

    Sort by first name completed

--- src/main/java/com/data/Sort.java
+++ src/main/java/com/data/Sort.java
@@ -9,29 +9,25 @@
 package com.data;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Sort {
 	
-	public void SortName() throws SQLException {
-		//connect to the database
-		Connection dbConnection = PQQuery.connectDB();
-		
-		//execute a query by selecting the columns 'First' and 'Last' from the table named Plot
-		Statement stmt = dbConnection.createStatement();
-		String string = "SELECT first, middle, last,  FROM Plot" + " ORDER BY first ASC";
-		ResultSet results = stmt.executeQuery(string);
-
-		//return results by column name
-		while (results.next()) {
-			String first = results.getString("First");
-			String last = results.getString("Last");
-			
-			System.out.println("First Name: " + first);
-			System.out.println("Last Name: " + last);
-		}
-		
-		if (stmt != null) {
-			dbConnection.close();
+	public void SortFirst(ArrayList<Plot> plots) throws SQLException {
+		int size = plots.size();
+		for (int i = 0; i < size -1; i++) {
+			for (int j = 0; j < size-i-1; j++) {
+				Plot iPlot = plots.get(j);
+				String iName = iPlot.getFirstName();
+				Plot jPlot = plots.get(j + 1);
+				String jName = jPlot.getFirstName();
+				
+				if (iName.compareTo(jName) > 0) {
+				Plot temp = iPlot;
+				plots.set(j, jPlot);
+				plots.set(j+1, temp);
+				}
+			}
 		}
 	}
 	
