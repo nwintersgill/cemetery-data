1edbaa56dd029735e32ce6794a37e95f1bb96935
Nathan Wintersgill <njw011@bucknell.edu>
2019-12-18 00:09:16 -0500

    integrate map into app, create iframe and new servlet

---
+++ src/main/java/com/servlet/MapServlet.java
@@ -0,0 +1,31 @@
+
+//based on http://www.eg.bucknell.edu/~mead/Java-tutorial/servlets/client-interaction/http-methods.html
+
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
+
+@WebServlet(urlPatterns="/map", name="searchHandler")
+public class MapServlet extends HttpServlet {
+
+    @Override
+    public void doGet (HttpServletRequest request,
+                       HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/resources/index_stMary.html");
+
+        dispatcher.forward(request, response);
+
+
+    }
+
+
+}
