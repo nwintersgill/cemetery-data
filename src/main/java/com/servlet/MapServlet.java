b3ecdb414678368330adfd1a7a2de45c94f2a283
Nathan Wintersgill <njw011@bucknell.edu>
2019-12-18 00:46:15 -0500

    print path to console

--- src/main/java/com/servlet/MapServlet.java
+++ src/main/java/com/servlet/MapServlet.java
@@ -4,12 +4,14 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.File;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 
 @WebServlet(urlPatterns="/map", name="mapHandler")
 public class MapServlet extends HttpServlet {
@@ -19,8 +21,12 @@ public class MapServlet extends HttpServlet {
                        HttpServletResponse response)
         throws ServletException, IOException
     {
+
+        ServletContext context = getServletContext();
+        String path = context.getRealPath(File.separator);
+        System.out.println("PATH = " + path);
         
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/resouces/templates/index.html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/templates/index.html");
 
         dispatcher.forward(request, response);
 
